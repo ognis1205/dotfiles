877ab097a5563ef4d88d6850dd36a69974d89dbe
ognis1205 <shingo.okawa.g.h.c@gmail.com>
2018-06-09 02:35:31 +0900

    add general settings for emacs.

--- init.el
+++ init.el
@@ -4,6 +4,14 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
+;; General settings.
+(when (boundp 'show-trailing-whitespace) (setq-default show-trailing-whitespace t))
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq backup-inhibited t)
+(setq next-line-add-newlines nil)
+(setq-default tab-width 4 indent-tabs-mode nil)
+(setq default-major-mode 'text-mode)
+
 ;;(unless (package-installed-p 'scala-mode)
 ;;(package-refresh-contents) (package-install 'scala-mode))
 ;;(unless (package-installed-p 'ensime)
