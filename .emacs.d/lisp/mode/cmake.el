a277b475a519dc2238ae1607b150cbf4a0862a6e
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2020-12-11 08:12:04 +0900

    start to reformat and fix coding conventions.

--- .emacs.d/lisp/mode/cmake.el
+++ .emacs.d/lisp/mode/cmake.el
@@ -2,31 +2,158 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun mode/cmake--hook ()
-  "Initialize makefile mode."
-  (subword-mode t)
-  (validate-setq indent-tabs-mode nil))
-
-(use-package cmake-mode
-  :if
-  (executable-find "cmake")
+(use-package cmake-mode :no-require t :ensure t
+  ;; Package Loading Section.
+  ;;:disabled
   :defer
+  ;;:demand
+
+  ;; Package Dependency Section.
+  :ensure-system-package
+  cmake
+  ;;:load-path
+  ;;:quelpa
+  ;;:pin
+  ;;:requires
+  ;;:after
+
+  ;; Package Mode Section.
+  ;;:delight
+  ;;:interpreter
+  ;;:hook
+  ;;:magic
+  ;;:magic-fallback
   :mode
   "\(CMakeLists\.txt|\.cmake\)$"
-  :hook
-  (cmake-mode-hook . mode/cmake--hook)
+
+  ;; Package Command Section.
+  ;;:commands
+  ;;:bind
+  ;;:bind-key
+
+  ;; Package Custom Section.
+  ;;:custom
+  ;;:custom-face
+
+  ;; Package Configuration Section.
+  ;;:if :when :unless
+  ;;:defines
+  :functions
+  indent-tabs-mode
+  ;;:preface
+  ;;:init
   :config
-  (use-package company-cmake
-    :after
-    (company)
-    :config
-    (add-to-list 'company-backends 'company-cmake))
-  (use-package cmake-font-lock
-    :hook
-    (cmake-mode-hook . cmake-font-lock-activate))
-  (use-package eldoc-cmake
-    :hook
-    (cmake-mode . eldoc-cmake-enable)))
+  (subword-mode t)
+  (setq indent-tabs-mode nil))
+
+(use-package cmake-font-lock :no-require t :ensure t
+  ;; Package Loading Section.
+  ;;:disabled
+  :defer
+  ;;:demand
+
+  ;; Package Dependency Section.
+  ;;:ensure-system-package
+  ;;:load-path
+  ;;:quelpa
+  ;;:pin
+  ;;:requires
+  ;;:after
+
+  ;; Package Mode Section.
+  ;;:delight
+  ;;:interpreter
+  :hook
+  (cmake-mode . cmake-font-lock-activate)
+  ;;:magic
+  ;;:magic-fallback
+  ;;:mode
+
+  ;; Package Command Section.
+  ;;:commands
+  ;;:bind
+  ;;:bind-key
+
+  ;; Package Custom Section.
+  ;;:custom
+  ;;:custom-face
+
+  ;; Package Configuration Section.
+  ;;:if :when :unless
+  ;;:defines
+  ;;:functions
+  ;;:preface
+  ;;:init
+  ;;:config
+  )
+
+
+(use-package eldoc-cmake :no-require t :ensure t
+  ;; Package Loading Section.
+  ;;:disabled
+  :defer
+  ;;:demand
+
+  ;; Package Dependency Section.
+  ;;:ensure-system-package
+  ;;:load-path
+  ;;:quelpa
+  ;;:pin
+  ;;:requires
+  ;;:after
+
+  ;; Package Mode Section.
+  ;;:delight
+  ;;:interpreter
+  :hook
+  (cmake-mode . eldoc-cmake-enable)
+  ;;:magic
+  ;;:magic-fallback
+  ;;:mode
+
+  ;; Package Command Section.
+  ;;:commands
+  ;;:bind
+  ;;:bind-key
+
+  ;; Package Custom Section.
+  ;;:custom
+  ;;:custom-face
+
+  ;; Package Configuration Section.
+  ;;:if :when :unless
+  ;;:defines
+  ;;:functions
+  ;;:preface
+  ;;:init
+  ;;:config
+  )
+
+;;(defun mode/cmake--hook ()
+;;  "Initialize makefile mode."
+;;  (subword-mode t)
+;;  (validate-setq indent-tabs-mode nil))
+;;
+;;(use-package cmake-mode
+;;  :if
+;;  (executable-find "cmake")
+;;  :defer
+;;  :mode
+;;  "\(CMakeLists\.txt|\.cmake\)$"
+;;  :hook
+;;  (cmake-mode-hook . mode/cmake--hook)
+;;  :config
+;;  (use-package company-cmake
+;;    :after
+;;    (company)
+;;    :config
+;;    (add-to-list 'company-backends 'company-cmake))
+;;  (use-package cmake-font-lock
+;;    :hook
+;;    (cmake-mode-hook . cmake-font-lock-activate))
+;;  (use-package eldoc-cmake
+;;    :hook
+;;    (cmake-mode . eldoc-cmake-enable)))
 
 (provide 'mode/cmake)
 
--- .emacs.d/lisp/mode/csv.el
+++ .emacs.d/lisp/mode/csv.el
@@ -2,22 +2,51 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun mode/csv--hook ()
-  "CSV mode hook."
+(use-package csv-mode :no-require t :ensure t
+  ;; Package Loading Section.
+  ;;:disabled
+  :defer
+  ;;:demand
+
+  ;; Package Dependency Section.
+  ;;:ensure-system-package
+  ;;:load-path
+  ;;:quelpa
+  ;;:pin
+  ;;:requires
+  ;;:after
+
+  ;; Package Mode Section.
+  ;;:delight
+  ;;:interpreter
+  ;;:hook
+  ;;:magic
+  ;;:magic-fallback
+  :mode
+  "\\.[CcTt][Ss][Vv]$"
+
+  ;; Package Command Section.
+  ;;:commands
+  ;;:bind
+  ;;:bind-key
+
+  ;; Package Custom Section.
+  :custom
+  (csv-separators '("," ";" "|" " " "\t"))
+  (csv-header-lines 1)
+  ;;:custom-face
+
+  ;; Package Configuration Section.
+  ;;:if :when :unless
+  :defines
+  truncate-lines
+  ;;:functions
+  ;;:preface
+  ;;:init
+  :config
   (setq truncate-lines t)
   (turn-off-auto-fill))
 
-(use-package csv-mode
-  :defer
-  :mode
-  "\\.[CcTt][Ss][Vv]$"
-  :init
-  (add-hook 'csv-mode-hook 'mode/csv--hook)
-  :config
-  (validate-setq
-   csv-separators '("," ";" "|" " " "\t")
-   csv-header-lines 1))
-
-(provide 'mode::csv)
+(provide 'mode/csv)
 
 ;;; csv.el ends here
