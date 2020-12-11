0dcb127669ff9ec96afd5cec95d600485c28274a
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2020-12-11 13:49:47 +0900

    remove redundant codes.

--- .emacs.d/lisp/mode/csv.el
+++ .emacs.d/lisp/mode/csv.el
@@ -3,46 +3,14 @@
 ;;; Code:
 
 (use-package csv-mode :no-require t :ensure t
-  ;; Package Loading Section.
-  ;;:disabled
   :defer
-  ;;:demand
-
-  ;; Package Dependency Section.
-  ;;:ensure-system-package
-  ;;:load-path
-  ;;:quelpa
-  ;;:pin
-  ;;:requires
-  ;;:after
-
-  ;; Package Mode Section.
-  ;;:delight
-  ;;:interpreter
-  ;;:hook
-  ;;:magic
-  ;;:magic-fallback
   :mode
   "\\.[CcTt][Ss][Vv]$"
-
-  ;; Package Command Section.
-  ;;:commands
-  ;;:bind
-  ;;:bind-key
-
-  ;; Package Custom Section.
   :custom
   (csv-separators '("," ";" "|" " " "\t"))
   (csv-header-lines 1)
-  ;;:custom-face
-
-  ;; Package Configuration Section.
-  ;;:if :when :unless
   :defines
   truncate-lines
-  ;;:functions
-  ;;:preface
-  ;;:init
   :config
   (setq truncate-lines t)
   (turn-off-auto-fill))
