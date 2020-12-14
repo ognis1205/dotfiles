e425c58605345762d05a5a8fde270c6073b85cd2
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2020-12-14 15:53:21 +0900

    add some interactives.

--- .emacs.d/lisp/init-interactives.el
+++ .emacs.d/lisp/init-interactives.el
@@ -7,13 +7,23 @@
   (interactive)
   (ux/template/insert "C++/atcoder.cc"))
 
+(defun user/insert-template-compile-flags-txt ()
+  "Insert compile_flags.txt template for C++."
+  (interactive)
+  (ux/template/insert "C++/compile_flags.txt"))
+
 (defun user/insert-template-atcoder-py ()
   "Insert AtCoder template for Python."
   (interactive)
   (ux/template/insert "Python/atcoder.py"))
 
+(defun user/insert-template-pyrightconfig-json ()
+  "Insert pyrightconfig.json template for Python."
+  (interactive)
+  (ux/template/insert "Python/pyrightconfig.json"))
+
 (defun user/insert-template-use-package-el ()
-  "Insert AtCoder template for Python."
+  "Insert use-package template for Emacs Lisp."
   (interactive)
   (ux/template/insert "EmacsLisp/use-package-template.el"))
 
